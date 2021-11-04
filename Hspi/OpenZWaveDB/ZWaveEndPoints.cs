6dd7372bf0637645597ff3f13a12fa4708332300
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2021-11-03 21:25:14 -0700

    Refactor

---
+++ Hspi/OpenZWaveDB/ZWaveEndPoints.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+#nullable enable
+
+namespace Hspi.OpenZWaveDB
+{
+    internal record ZWaveEndPoints
+    {
+        public IReadOnlyList<ZWaveCommandClass>? CommandClass { get; init; }
+    }
+}
--- Hspi/OpenZWaveDB/ZWaveInformation.cs
+++ Hspi/OpenZWaveDB/ZWaveInformation.cs
@@ -7,48 +7,6 @@ using System.Linq;
 
 namespace Hspi.OpenZWaveDB
 {
-    internal record ZWaveEndPoints
-    {
-        public IReadOnlyList<ZWaveCommandClass>? CommandClass { get; init; }
-    }
-
-    internal record ZWaveCommandClass
-    {
-        [JsonProperty("commandclass_name")]
-        public string? Name { get; init; }
-
-        public IReadOnlyList<ZWaveCommandClassChannel>? Channels { get; init; }
-
-        [JsonIgnore]
-        public bool IsSetCommand => Name == "COMMAND_CLASS_CONFIGURATION";
-    }
-
-    internal record ZWaveCommandClassChannel
-    {
-        [JsonProperty("config")]
-        public string? Config { get; init; }
-
-        public string? Label { get; init; }
-        public string? Overview { get; init; }
-
-        [JsonIgnore]
-        public int? ParameterId
-        {
-            get
-            {
-                var list = Config?.Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
-                if (list != null && list.Length == 2)
-                {
-                    if (int.TryParse(list[1], out var value))
-                    {
-                        return value;
-                    }
-                }
-                return null;
-            }
-        }
-    }
-
     internal record ZWaveInformation
     {
         [JsonProperty("database_id")]

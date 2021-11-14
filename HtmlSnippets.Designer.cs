f0112d2268f7cf99f44859e805591100bebf272b
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2021-11-13 17:54:03 -0800

    Assert Changes

--- HtmlSnippets.Designer.cs
+++ HtmlSnippets.Designer.cs
@@ -19,7 +19,7 @@ namespace Hspi {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class HtmlSnippets {
@@ -85,6 +85,16 @@ namespace Hspi {
         internal static string ClickRefreshButtonScript {
             get {
                 return ResourceManager.GetString("ClickRefreshButtonScript", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
